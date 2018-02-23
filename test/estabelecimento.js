0bee1cf6b986380d0691ace9cc4c1ecf844f343a
Yasser Nascimento <yassern@protonmail.com>
2018-02-22 23:13:41 -0300

    test post evaluation

--- test/estabelecimento.js
+++ test/estabelecimento.js
@@ -11,7 +11,7 @@ let should = chai.should();
 chai.use(chaiHttp);
 
 describe("/GET estabelecimentos", () => {
-  it("it should GET all estabelecimentos", done => {
+  it("it should GET all establishments", done => {
     chai
       .request(server)
       .get("/api/estabelecimentos")
@@ -19,6 +19,25 @@ describe("/GET estabelecimentos", () => {
         res.should.have.status(200);
         res.body.should.be.a("array");
         res.body.length.should.be.eql(2226);
+        done();
+      });
+  });
+
+  it("it should POST a evaluation", done => {
+    let evaluation = {
+      type: "qualidadeAtendimento",
+      nota: "4",
+      bday: "2018-02-18",
+      turno: "Tarde",
+      comentario: "comentário"
+    };
+
+    chai
+      .request(server)
+      .post("/api/estabelecimentos/2607832")
+      .send(evaluation)
+      .end((err, res) => {
+        res.should.have.status(200);
         done();
       });
   });
